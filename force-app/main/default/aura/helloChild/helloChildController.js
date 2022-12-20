81bb58ed21d9f7267828690e3450efd434f302fe
Rohit Macherla <rohit.macherla@gmail.com>
2022-12-20 17:18:00 +0530

    list iteration

--- force-app/main/default/aura/helloChild/helloChildController.js
+++ force-app/main/default/aura/helloChild/helloChildController.js
@@ -3,6 +3,16 @@
         const var1 = component.get("v.greeting");
         console.log('var1 :' + var1);
         console.log('handleClick is called, event is :' + event.target.value);
+
+        const vehicles = [{"name": "car", "type": "short"}, {"name": "train", "type": "long"}, {"name": "bus", "type": "short"}];
+        component.set("v.vehicles", vehicles);
+
         helper.helperMethod();
+    },
+    
+    addOneMore : function(component, event, helper) {
+        const vehicles = component.get("v.vehicles");
+        vehicles.push({"name": "ship", "type": "long"});
+        component.set("v.vehicles", vehicles);
     }
 })
